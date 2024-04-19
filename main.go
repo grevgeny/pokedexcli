57fd24c4ddd452b6bef7e3647717a6e2b936f6de
Evgeny Grigorenko <eu.grigorenko@gmail.com>
2024-04-19 18:12:30 +0400

    feat: echo repl

--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type replCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func commandHelp() error {
+	panic("")
+}
+
+func commandExit() error {
+	panic("")
+}
+
+func getCommands() map[string]replCommand {
+	return map[string]replCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		}}
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("Pokedex > ")
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+		fmt.Print("Pokedex > ")
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading input: %s", err)
+	}
 }
